f7214db5f504e54f90981f8bc6c83db42acc1009
Zababurin <s.zababurin.v@gmail.com>
2021-01-22 20:07:27 +0300

    update

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
 let app = require('./index.js')
-
-app.listen(3003, function () { console.log('listening on http://localhost:3003'); });
+let system = {
+  port: process.env.PORT || 3003,
+  pid:  process.pid
+}
+app.listen(3003, function () { console.log('server mongo: ',system); });
